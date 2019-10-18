c20b3c5e9802c8caf6cbd31445ab34167c42da2f
Hugo Feldstein <16608904+Taloud@users.noreply.github.com>
2019-10-18 13:14:14 +0200

    first-commi

---
+++ app/pages/_app.js
@@ -0,0 +1,17 @@
+import App from 'next/app'
+import React from 'react'
+import withApolloClient from '../src/lib/apollo/with-apollo-client'
+import { ApolloProvider } from 'react-apollo'
+
+class MyApp extends App {
+  render () {
+    const { Component, pageProps, apolloClient } = this.props
+    return (
+        <ApolloProvider client={apolloClient}>
+          <Component {...pageProps} />
+        </ApolloProvider>
+    )
+  }
+}
+
+export default withApolloClient(MyApp)
