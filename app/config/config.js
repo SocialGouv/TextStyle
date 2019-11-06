d0069caefca5aa1fa14d881a8499df0929bed0ea
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-11-06 11:09:45 +0100

    feat: add docker-compose setup
     - add /elastic and /graphql-engine proxies into Next.js server. this reduce complexity of dealing with various hostnames
     - add sample docker-compose.override.prod.yaml to enable app build in production
     - add app/Dockerfile
    
    by running `docker-compose up` one should be able to get the full setup

--- app/config/config.js
+++ app/config/config.js
@@ -1,2 +1,2 @@
-export const GRAPHQL_URL = 'http://localhost:8080/v1/graphql'
-export const PER_PAGE = 5
+export const GRAPHQL_URL = "/graphql-engine/v1/graphql"
+export const PER_PAGE = 5;
---
+++ app/server.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-console */
+const express = require("express");
+const next = require("next");
+const proxyMiddleware = require("http-proxy-middleware");
+
+const ELASTIC_URL = process.env.ELASTIC_URL || "http://127.0.0.1:9200";
+const GRAPHQL_URL = process.env.GRAPHQL_URL || "http://127.0.0.1:8080";
+const PORT = parseInt(process.env.PORT, 10) || 3000;
+const NODE_ENV = process.env.NODE_ENV;
+
+const proxies = {
+  "/elastic": {
+    target: ELASTIC_URL,
+    pathRewrite: { "^/elastic": "/" },
+    changeOrigin: true
+  },
+  "/graphql-engine": {
+    target: GRAPHQL_URL,
+    pathRewrite: { "^/graphql-engine": "/" },
+    changeOrigin: true
+  }
+};
+
+
+const dev = NODE_ENV !== "production";
+const app = next({
+  dev
+});
+
+const handle = app.getRequestHandler();
+
+let server;
+app
+  .prepare()
+  .then(() => {
+    server = express();
+
+    // Set up the proxy.
+    Object.keys(proxies).forEach(function(context) {
+      server.use(proxyMiddleware(context, proxies[context]));
+    });
+
+    // Default catch-all handler to allow Next.js to handle all other routes
+    server.all("*", (req, res) => handle(req, res));
+
+    server.listen(PORT, err => {
+      if (err) {
+        throw err;
+      }
+      console.log(`> Ready on http://127.0.0.1:${PORT} [${NODE_ENV}]`);
+    });
+  })
+  .catch(err => {
+    console.log("An error occurred, unable to start the server");
+    console.log(err);
+  });
