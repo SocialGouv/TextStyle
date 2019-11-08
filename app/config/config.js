6db1e570ff11e0828cab7c3e0c334850979ebbf7
Hugo Feldstein <16608904+Taloud@users.noreply.github.com>
2019-11-08 12:02:45 +0100

    fix: missing ';' in config

--- app/config/config.js
+++ app/config/config.js
@@ -1,6 +1,6 @@
 // export const GRAPHQL_URL = "/graphql-engine/v1/graphql" / can't use absolute path
 // export const GRAPHQL_URL = "http://localhost:8080/v1/graphql"; for local use only
 
-export const GRAPHQL_URL = "//40.89.168.163:8082/v1/graphql"
+export const GRAPHQL_URL = "//40.89.168.163:8082/v1/graphql";
 
 export const PER_PAGE = 5;
