61496c6f2c8eea6dc4fed6acec80b36ad86f5d25
Hugo Feldstein <16608904+Taloud@users.noreply.github.com>
2020-01-20 10:26:00 +0100

    fix: lint error

--- app/pages/profile.js
+++ app/pages/profile.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Image } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
+// import Button from "react-bootstrap/Button";
 import { withAuthSync, getJwt } from "../utils/auth";
 
 import Col from "react-bootstrap/Col";
