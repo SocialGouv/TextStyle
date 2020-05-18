fd8401932caedb16988f844b6263c0763b059c6b
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-05-18 12:10:03 +0200

    fix: use dev connection when no env.POSTGRES_URL

--- api/knexfile.js
+++ api/knexfile.js
@@ -1,16 +1,16 @@
 // Update with your config settings.
-// const password = "p'e.(BT3j7QC,z-<";
-// const connection_url = `postgres://postgres:${password}@localhost:5434/postgres`;
+const DEV_POSTGRES_PASSWORD = "p'e.(BT3j7QC,z-<";
+const DEV_POSTGRES_URL = `postgres://postgres:${DEV_POSTGRES_PASSWORD}@localhost:5434/postgres`;
 
 const connection_url =
   typeof window !== "undefined"
     ? `postgres://postgres:p'e.(BT3j7QC,z-<@localhost:5432/postgres`
-    : process.env.POSTGRES_URL;
+    : process.env.POSTGRES_URL || DEV_POSTGRES_URL;
 
 module.exports = {
   client: "pg",
   connection: connection_url,
   migrations: {
-    directory: __dirname + "/db/migrations"
-  }
+    directory: __dirname + "/db/migrations",
+  },
 };
